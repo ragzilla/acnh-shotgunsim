ec9c055a96aefe09b0816f1c40267bad44bd5340
Matt Addison <matt@evilgeni.us>
2020-05-15 00:39:55 +0000

    initial

---
+++ shotgunsim.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+
+import numpy
+
+class Flower:
+	genes = None
+
+	def __init__(self, genes):
+		self.genes = genes
+
+	def breed(self, partner):
+		return
+
+class Field:
+	matrix = None
+
+	def __init__(self, configuration):
+		return
+
+class Feeder:
+	genepool = None
+	probpool = None
+
+	def __init__(self, genepool, probpool):
+		return
+
+def main():
+	print("hello, world")
+	return
+
+if __name__ == "__main__":
+    main()
