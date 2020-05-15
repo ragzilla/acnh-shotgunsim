c33eea3dc2b776380a249c6b3d7568d3042b2056
Matt Addison <matt@evilgeni.us>
2020-05-15 03:19:16 +0000

    field population done

--- field.py
+++ field.py
@@ -8,11 +8,28 @@ class Field:
 	def __init__(self, name, configuration):
 		self.name   = name
 		self.layout = configuration
+		self.x      = len(self.layout[0])
+		self.y      = len(self.layout)
+		self.matrix = [ [ None for i in range(self.x) ] for j in range(self.y) ] 
+		print(self.name, self.matrix, self.y, self.x)
 		return
 
 	def place(self, flower1, flower2 = None):
 		"""place 1-2 flowers in the layout from the feeder"""
+		### find an open spot
 		print("Field(\"{}\").place:".format(self.name), flower1, flower2)
+		flowers = [flower1,]
+		if flower2 != None:
+			flowers.append(flower2)
+		for y in range(self.y):
+			if len(flowers) == 0:
+				break
+			for x in range(self.x):
+				if self.layout[y][x] == 1 and self.matrix[y][x] == None:
+					self.matrix[y][x] = flowers.pop()
+				if len(flowers) == 0:
+					break
+		print(self.matrix)
 		return
 
 	def run(self):
